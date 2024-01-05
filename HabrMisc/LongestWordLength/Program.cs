ed5dd6931b5231f8e3398435aadb22ccfc3c5c68
Alexander Radchenko <radchenkosasha@gmail.com>
2024-01-05 13:54:44 +0700

    Job.ShortRun

--- HabrMisc/LongestWordLength/Program.cs
+++ HabrMisc/LongestWordLength/Program.cs
@@ -1,12 +1,17 @@
 ﻿// See https://habr.com/ru/articles/782250/ for more information
 
 using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
+using Perfolizer.Horology;
 
 var config = ManualConfig.Create(DefaultConfig.Instance)
 //var config = new DebugInProcessConfig()
     //.WithOption(ConfigOptions.JoinSummary, true)
     .WithOption(ConfigOptions.StopOnFirstError, true)
+    .AddJob(Job.ShortRun
+        .WithIterationTime(TimeInterval.Millisecond * 100)
+        )
     ;
 
 BenchmarkRunner.Run(typeof(Program).Assembly, config);
