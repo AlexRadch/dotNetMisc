9652429f48e610f142295b86b2ad398a0d14e784
Alexander Radchenko <radchenkosasha@gmail.com>
2024-01-09 13:43:42 +0700

    ToOrdinalString

--- SocialNet/AddOrdinal/Program.cs
+++ SocialNet/AddOrdinal/Program.cs
@@ -5,10 +5,12 @@ using System;
 using System.Linq;
 using System.Numerics;
 
-foreach (var i in Enumerable.Range(-1, 25))
+#nullable enable
+
+foreach (var i in Enumerable.Range(-1, 29))
     Console.WriteLine(Helpers.ToOrdinalString(i));
 
-foreach (var i in Enumerable.Range(12_000, 25).Select(x => new BigInteger(x)))
+foreach (var i in Enumerable.Range(12_000, 29).Select(x => new BigInteger(x)))
     Console.WriteLine(Helpers.ToOrdinalString(i, "N0"));
 
 public static class Helpers
@@ -19,10 +21,11 @@ public static class Helpers
     => value.ToString(format, formatProvider) + (
         value <= TInt.Zero
             ? ""
-            : int.CreateTruncating(value % TInt.CreateChecked(10)) is var number &&
-                ((number is > 3 or < 1) || int.CreateTruncating(value % TInt.CreateChecked(100)) > 10)
+            : int.CreateTruncating(value % TInt.CreateChecked(10)) is var ones &&
+                ((ones is > 3 or < 1) || 
+                    (int.CreateTruncating(value % TInt.CreateChecked(100)) is <= 13 and >= 11))
                 ? "th"
-                : number switch
+                : ones switch
                 {
                     1 => "st",
                     2 => "nd",
